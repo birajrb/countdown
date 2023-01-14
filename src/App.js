c689cbd6a6a14c111936d3044f999be5bc54323a
Luffy <birajr0@gmail.com>
2023-01-14 14:53:00 +0545

    Fix count bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   const calculateTimeLeft = () => {
     var today = new Date();
-    var targetTime = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7, 13, 0, 0);
+    var targetTime = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 5, 13, 0, 0);
 
     var difference = targetTime - today;
     let timeLeft = {};
